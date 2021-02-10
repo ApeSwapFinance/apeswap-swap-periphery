4814a072635a10a55041334763456df98598a962
defifofum <defifofum0@gmail.com>
2021-02-10 00:14:47 -0500

    cleanup

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -2,8 +2,7 @@ const ApeRouter = artifacts.require("PancakeRouter");
 
 const { config } = require('./migration-config');
 
-//constructor(address _factory, address _WETH) public {
-
 module.exports = function (deployer, network, accounts) {
+  //constructor(address _factory, address _WETH) public {
   deployer.deploy(ApeRouter, config[network].factoryAddress, config[network].WBNBAddress);
 };
