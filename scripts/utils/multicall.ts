78939b7220cb261c9bd3f07a6ac87e4c1c7250d1
defifofum <defifofum0@gmail.com>
2021-12-16 18:19:08 -0500

    feat: Add script helpers

---
+++ scripts/utils/multicall.ts
@@ -0,0 +1,38 @@
+
+import { Interface } from '@ethersproject/abi'
+import { Contract } from '@ethersproject/contracts'
+
+export function chunkArray(arr: any[], len: number) {
+    let chunks = [];
+    let i = 0;
+    let n = arr.length;
+
+    while (i < n) {
+        chunks.push(arr.slice(i, i += len));
+    }
+
+    return chunks;
+}
+
+export interface Call {
+    address: string // Address of the contract
+    functionName: string // Function name on the contract (exemple: balanceOf)
+    params?: any[] // Function params
+}
+
+export async function multicall(multi: Contract, abi: any[], calls: Call[], maxCallsPerTx = 1000) {
+    const itf = new Interface(abi)
+
+    const chunkedCalls = chunkArray(calls, maxCallsPerTx);
+
+    let finalData: any[] = []
+    for (const currentCalls of chunkedCalls) {
+        const calldata = currentCalls.map((call) => [call.address.toLowerCase(), itf.encodeFunctionData(call.functionName, call.params)])
+        const { returnData } = await multi.callStatic.aggregate(calldata);
+        const res = returnData.map((call: any, i: number) => itf.decodeFunctionResult(currentCalls[i].functionName, call))
+        finalData = [...finalData, ...res];
+    }
+
+    return finalData
+}
+
