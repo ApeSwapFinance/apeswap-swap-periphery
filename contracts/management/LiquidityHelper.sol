6ef2143d1ed5dd4c21cde4430cad9a898239c420
defifofum <defifofum0@gmail.com>
2022-04-21 10:41:07 -0400

    refactor: LiquidityHelper to inclue structs

--- contracts/management/LiquidityHelper.sol
+++ contracts/management/LiquidityHelper.sol
@@ -1,5 +1,6 @@
 // SPDX-License-Identifier: MIT
 pragma solidity =0.6.6;
+pragma experimental ABIEncoderV2;
 
 import '../interfaces/IApeFactory.sol';
 import '../interfaces/IApePair.sol';
@@ -10,138 +11,102 @@ contract LiquidityHelper {
     using SafeMath for uint256;
     IApeFactory public factory;
 
+    struct PairInfo {
+        uint256 totalLpSupply;
+        IERC20 token0;
+        string token0Symbol;
+        uint256 token0Balance;
+        IERC20 token1;
+        string token1Symbol;
+        uint256 token1Balance;
+    }
+
+    struct LiquidityOutInfo {
+        uint256 totalLpSupply;
+        IERC20 token0;
+        string token0Symbol;
+        uint256 token0Out;
+        IERC20 token1;
+        string token1Symbol;
+        uint256 token1Out;
+    }
+
     constructor(address factoryAddress) public {
         factory = IApeFactory(factoryAddress);
     }
 
     /// @notice Provide two tokens and this will find the pair address related and return useful values
-    /// @param token0 Address of token0
-    /// @param token1 Address of token1
-    /// @return totalLpSupply The total supply of LP tokens minted for the pair
-    /// @return token0 token0 as registered on the pair contract
-    /// @return token0Symbol symbol of token0
-    /// @return token0Balance The total balance of token0 in the pair
-    /// @return token1 token1 as registered on the pair contract
-    /// @return token1Symbol symbol of token1
-    /// @return token1Balance The total balance of token1 in the pair
-    function getPairBalances(address _tokenA, address _tokenB)
+    /// @param tokenA Address of token0
+    /// @param tokenB Address of token1
+    /// @return pairInfo PairInfo struct based on provided inputs
+    function getPairBalances(address tokenA, address tokenB)
         public
         view
-        returns (
-            uint256 totalLpSupply,
-            IERC20 token0, 
-            string memory token0Symbol, 
-            uint256 token0Balance, 
-            IERC20 token1, 
-            string memory token1Symbol, 
-            uint256 token1Balance
-        )
+        returns (PairInfo memory pairInfo)
     {
-        address pair = factory.getPair(_tokenA, _tokenB);
-        return getPairBalances(pair);
+        address pair = factory.getPair(tokenA, tokenB);
+        pairInfo = getPairBalances(pair);
     }
 
     /// @notice Provide a pair address and this will find the pair address related and return useful values
     /// @param pairAddress Address of the pair contract
-    /// @return totalLpSupply The total supply of LP tokens minted for the pair
-    /// @return token0 token0 as registered on the pair contract
-    /// @return token0Symbol symbol of token0
-    /// @return token0Balance The total balance of token0 in the pair
-    /// @return token1 token1 as registered on the pair contract
-    /// @return token1Symbol symbol of token1
-    /// @return token1Balance The total balance of token1 in the pair
+    /// @return pairInfo PairInfo struct based on provided inputs
     function getPairBalances(address pairAddress)
         public
         view
-        returns (
-            uint256 totalLpSupply,
-            IERC20 token0, 
-            string memory token0Symbol, 
-            uint256 token0Balance, 
-            IERC20 token1, 
-            string memory token1Symbol, 
-            uint256 token1Balance
-        )
+        returns (PairInfo memory pairInfo)
     {
         IApePair apePair = IApePair(pairAddress);
-        token0 = IERC20(apePair.token0());
-        token0Symbol = token0.symbol();
-        token1 = IERC20(apePair.token1());
-        token1Symbol = token1.symbol();
+        pairInfo.token0 = IERC20(apePair.token0());
+        pairInfo.token0Symbol = pairInfo.token0.symbol();
+        pairInfo.token1 = IERC20(apePair.token1());
+        pairInfo.token1Symbol = pairInfo.token1.symbol();
 
-        totalLpSupply = apePair.totalSupply();
-        token0Balance = token0.balanceOf(pairAddress);
-        token1Balance = token1.balanceOf(pairAddress);
+        pairInfo.totalLpSupply = apePair.totalSupply();
+        pairInfo.token0Balance = pairInfo.token0.balanceOf(pairAddress);
+        pairInfo.token1Balance = pairInfo.token1.balanceOf(pairAddress);
     }
 
     /// @notice Find the token outputs for unwrapping LP tokens
     /// @param tokenA Address of the token0 contract
     /// @param tokenB Address of the token1 contract
     /// @param lpBalance Amount of LP tokens to unwrap
-    /// @return totalLpSupply Total supply of LP tokens for the pair
-    /// @return token0 token0 as registered on the pair contract
-    /// @return token0Symbol symbol of token0
-    /// @return token0Out The output amount of token0
-    /// @return token1 token1 as registered on the pair contract
-    /// @return token1Symbol symbol of token1
-    /// @return token1Out The output amount of token1
-    function getLiquidityAmountsOut(address tokenA, address tokenB, uint256 lpBalance)
+    /// @return liquidityOutInfo LiquidityOutInfo based on input data
+    function getLiquidityAmountsOut(
+        address tokenA,
+        address tokenB,
+        uint256 lpBalance
+    )
         public
         view
-        returns (
-            uint256 totalLpSupply,
-            IERC20 token0,
-            string memory token0Symbol, 
-            uint256 token0Out,
-            IERC20 token1,
-            string memory token1Symbol, 
-            uint256 token1Out
-        )
+        returns (LiquidityOutInfo memory liquidityOutInfo)
     {
         address pair = factory.getPair(tokenA, tokenB);
-        return getLiquidityAmountsOut(pair, lpBalance);
+        liquidityOutInfo = getLiquidityAmountsOut(pair, lpBalance);
     }
 
     /// @notice Find the token outputs for unwrapping LP tokens
     /// @param pairAddress Address of the pair contract
     /// @param lpBalance Amount of LP tokens to unwrap
-    /// @return totalLpSupply Total supply of LP tokens for the pair
-    /// @return token0 token0 as registered on the pair contract
-    /// @return token0Symbol symbol of token0
-    /// @return token0Out The output amount of token0
-    /// @return token1 token1 as registered on the pair contract
-    /// @return token1Symbol symbol of token1
-    /// @return token1Out The output amount of token1
+    /// @return liquidityOutInfo LiquidityOutInfo based on input data
     function getLiquidityAmountsOut(address pairAddress, uint256 lpBalance)
         public
         view
-        returns (
-            uint256 totalLpSupply,
-            IERC20 token0,
-            string memory token0Symbol, 
-            uint256 token0Out,
-            IERC20 token1,
-            string memory token1Symbol, 
-            uint256 token1Out
-        )
+        returns (LiquidityOutInfo memory liquidityOutInfo)
     {
-        (
-            uint256 totalLpSupply_, 
-            IERC20 token0_,
-            string memory token0Symbol_,
-            uint256 token0Balance, 
-            IERC20 token1_,
-            string memory token1Symbol_,
-            uint256 token1Balance
-        ) = getPairBalances(pairAddress);
+        PairInfo memory pairInfo = getPairBalances(pairAddress);
 
-        totalLpSupply = totalLpSupply_;
-        token0 = token0_;
-        token0Symbol = token0Symbol_;
-        token1 = token1_;
-        token1Symbol = token1Symbol_;
+        uint256 token0Out = lpBalance.mul(pairInfo.token0Balance) / (pairInfo.totalLpSupply);
+        uint256 token1Out = lpBalance.mul(pairInfo.token1Balance) / (pairInfo.totalLpSupply);
 
-        token0Out = lpBalance.mul(token0Balance) / (totalLpSupply_);
-        token1Out = lpBalance.mul(token1Balance) / (totalLpSupply_);
+        liquidityOutInfo = LiquidityOutInfo(
+            pairInfo.totalLpSupply,
+            pairInfo.token0,
+            pairInfo.token0Symbol,
+            token0Out,
+            pairInfo.token1,
+            pairInfo.token1Symbol,
+            token1Out
+        );
     }
 }
