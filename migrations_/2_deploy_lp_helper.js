7737fb064acfa1d0ce356dc2d50ec949a8b55c13
defifofum <defifofum0@gmail.com>
2022-05-04 13:48:55 -0400

    refactor: Remove LPFeeManager contracts as they have been moved to `apeswap-lp-fee-manager` repo

--- migrations_/2_deploy_lp_helper.js
+++
@@ -1,8 +0,0 @@
-const LiquidityHelper = artifacts.require("LiquidityHelper");
-const { getNetworkConfig } = require('../migration-config');
-
-
-module.exports = async function (deployer, network, accounts) {
-  const { factoryAddress, wrappedAddress } = getNetworkConfig(network, accounts);
-  await deployer.deploy(LiquidityHelper, factoryAddress);
-};
